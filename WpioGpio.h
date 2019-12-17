5b0a9a629e9193767e8d02c5f82e5ac5764e50b2
j8kin <emk161278@gmail.com>
2019-12-17 22:06:03 +0300

    add wiringPI mapping + 2 buttons + 1 diode

---
+++ WpioGpio.h
@@ -0,0 +1,6 @@
+/*
+define GPIO inputs as WiringPi to use GPIO number instead of WiringPi magic numbers
+*/
+#define GPIO17 0
+#define GPIO27 2
+#define GPIO22 3
--- tapper.c
+++ tapper.c
@@ -1,25 +1,69 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <wiringPi.h>
+#include "WpioGpio.h"
 
 int main() {
     printf("Amina's tapper simulator. RaspberryPi\n");
     wiringPiSetup () ;
 
-    pinMode (12, INPUT) ;
+    pinMode(GPIO17, OUTPUT);
+    pinMode(GPIO27, INPUT);
+    pinMode(GPIO22, INPUT);
     
-    // int i = 0;
+    int i = 0;
+    int j = 0;
+    char morseLetter[256] ="\0";
+    int cSymbol = 0;;
     while(1) {
-        int curDotKeyPressed = digitalRead(12);
-        if (curDotKeyPressed == 1) {
-            printf("\nDot Key down\n");
+        if (j == 5) {
+            digitalWrite(GPIO17, HIGH); // delay(500);
         }
-        
-        // ++i;
-        // if (i == 10) {
-        //     printf(".");
-        //     i = 0;
-        // }
-        usleep(100); // 10Hz
+        if (j==10) {
+            digitalWrite(GPIO17, LOW); // delay(500);
+            j = -1;
+        }
+        ++j;
+
+        int curDotKeyPressed = digitalRead(GPIO27);
+        int curDashKeyPressed = digitalRead(GPIO22);
+
+        if (curDotKeyPressed == 1) {
+            // printf("\nDot Key down\n");
+            ++i;
+        }
+        else {
+            if (i>0) {
+                // printf("Dot Key down for %d sec\n", i/10);
+                // printf(".");
+                morseLetter[cSymbol++]='.';
+                morseLetter[cSymbol]='\0';
+                i = 0;
+            }
+        }
+
+        if (curDashKeyPressed == 1) {
+            // printf("\Dash Key down\n");
+            ++i;
+        }
+        else {
+            if (i>0) {
+                // printf("Dash Key down for %d sec\n", i/10);
+                // printf("-");
+                morseLetter[cSymbol++]='-';
+                morseLetter[cSymbol]='\0';
+
+                i = 0;
+            }
+        }
+
+        if (cSymbol > 5) {
+            printf("%s\n", morseLetter);
+            cSymbol = 0;
+            morseLetter[cSymbol]='\0';
+        }
+        delay(100); // 10Hz
     }
+
+    return 0;
 }
